b49b248fa263bf8f40988e08632948a12f495091
“[Lewis <simbilewis@gmail.com>
2019-06-08 23:17:53 +0200

    sec

--- EasCook AndrApp/app/src/main/java/net/calc/eascook/MainActivity.java
+++ EasCook AndrApp/app/src/main/java/net/calc/eascook/MainActivity.java
@@ -2,12 +2,21 @@ package net.calc.eascook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.ToolbarWidgetWrapper;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.action_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("EasCook");
+        toolbar.setLogo(R.drawable.logo);
+
     }
 }
